4c7f9bb4d3b3ff8732f61af1a8529bfcaa88c522
Arturo <arturocs@protonmail.com>
2021-12-16 21:51:40 +0100

    Added example actix project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+
+#[get("/{name}")]
+async fn index(web::Path(name): web::Path<String>) -> impl Responder {
+    format!("Hello {}!", name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let url = "0.0.0.0:8080";
+    println!("Serving at: {}", url);
+    HttpServer::new(|| App::new().service(index))
+        .bind(url)?
+        .run()
+        .await
 }
