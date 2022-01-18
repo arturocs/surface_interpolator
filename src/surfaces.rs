2b997c262b6d5cbd320df2e5b0fa94d3e34a1af2
ajalba <ajalba@correo.ugr.es>
2022-01-18 19:01:25 +0100

    oups

--- src/surfaces.rs
+++ src/surfaces.rs
@@ -7,5 +7,5 @@ trait BezierSurface {
     fn decasteljau_surfaces(points: &[2DPoint]) -> &[2DPoint]
     fn degree_elevation(points: &[2DPoint], degree: i32) -> &[2DPoint]
     fn interpolant_surfaces(left: f32, right: f32, step_long: f32, function: String) -> &[3DPoint]
-    // el de superficies con calma, tiene como 30 funciones que definir antes
+    // easy on the surfaces one, there is a lot of things to define first
 }
