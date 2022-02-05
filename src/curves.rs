cff063d8d76e23d5239985656f0bf3a070f14a8f
Arturo <arturocs@protonmail.com>
2022-02-05 21:42:26 +0100

    Copy changes from actixV4beta branch, and apply some fixes

--- src/curves.rs
+++ src/curves.rs
@@ -1,28 +1,104 @@
-pub struct Point2D {
+use bio::stats::combinatorics;
+use rusymbols::{core, Expression};
+pub struct F2DPoint {
     x: f32,
     y: f32,
 }
 trait BezierCurve {
     // For the moment is okay on 2D, extension to 3D is easy
-    fn bernstein_base_curve(points: &[Point2D]) -> Vec<Point2D>;
-    fn decasteljau(points: &[Point2D]) -> Vec<Point2D>;
-    fn curve_fix_degree(points: &[Point2D], degree: i32) -> Vec<Point2D>;
-    fn curve_from_function(
-        left: f32,
-        right: f32,
-        degree: i32,
-        function: fn(f32) -> f32,
-    ) -> Vec<Point2D>;
+    fn bernstein_base_curve(points: &[F2DPoint]) -> Expression;
+    fn j_base_element_degree_n(j: i32, n: i32) -> Expression;
+    fn decasteljau_method(points: &[F2DPoint]) -> Expression;
+    fn curve_fix_degree(points: &[F2DPoint], degree: i32) -> Vec<F2DPoint>;
+    fn curve_from_function(left: f32, right: f32, degree: i32, function: String) -> Vec<F2DPoint>;
     fn interpolant_cuadratic_class_1(
         left: f32,
         right: f32,
         step_long: f32,
-        function: fn(f32) -> f32,
-    ) -> Vec<Point2D>;
+        function: String,
+    ) -> Vec<F2DPoint>;
     fn interpolant_cubic_class_2(
         left: f32,
         right: f32,
         step_long: f32,
-        function: fn(f32) -> f32,
-    ) -> Vec<Point2D>;
+        function: String,
+    ) -> Vec<F2DPoint>;
+}
+
+impl BezierCurve for F2DPoint {
+    fn decasteljau_method(points: &[F2DPoint]) -> Expression {
+        let degree = points.len();
+        let mut exp_iteration = Vec::new();
+        for j in 1..degree {
+            let mut curve_degree_j = Vec::new();
+            // Review this, variable n does not exists
+            //for i in 1..(n - j + 1) {
+            for i in 1..(degree - j + 1) {
+                let x = Expression::new_var("x");
+                // Expression::new_val() expects a f64 as argument, not a F2DPoint
+                //let first_point = Expression::new_val(points[i - 1]);
+                //let second_point = Expression::new_val(points[i]);
+                let first_point = todo!();
+                let second_point = todo!();
+                let one = Expression::new_val(-1.0);
+                let one_minus_x = Expression::new(one, x, core::Actions::Sub);
+                let first_monome = Expression::new(first_point, one_minus_x, core::Actions::Mul);
+                let second_monome = Expression::new(second_point, x, core::Actions::Mul);
+                let decasteljau_polynom =
+                    Expression::new(first_monome, second_monome, core::Actions::Mul);
+                curve_degree_j.push(decasteljau_polynom);
+            }
+            exp_iteration = curve_degree_j.clone();
+            curve_degree_j.clear();
+        }
+        return exp_iteration[0].clone();
+    }
+    fn j_base_element_degree_n(j: i32, n: i32) -> Expression {
+        let j_exp = Expression::new_val(j as f64);
+        let n_exp = Expression::new_val(n as f64);
+        let t = Expression::new_var("t");
+        let one = Expression::new_val(1.0);
+        let one_minus_t = Expression::new(one, t.clone(), core::Actions::Sub);
+        let combinations = Expression::new_val(combinatorics::combinations(n as u64, j as u64));
+        let n_minus_j = Expression::new(n_exp, j_exp.clone(), core::Actions::Sub);
+        let first_part = Expression::new(combinations, t.pow(j_exp), core::Actions::Mul);
+        let base_polynom =
+            Expression::new(first_part, one_minus_t.pow(n_minus_j), core::Actions::Mul);
+        return base_polynom;
+    }
+
+    fn bernstein_base_curve(points: &[F2DPoint]) -> Expression {
+        todo!()
+    }
+
+    fn curve_fix_degree(points: &[F2DPoint], degree: i32) -> Vec<F2DPoint> {
+        todo!()
+    }
+
+    fn curve_from_function<'a>(
+        left: f32,
+        right: f32,
+        degree: i32,
+        function: String,
+    ) -> Vec<F2DPoint> {
+        todo!()
+    }
+
+    fn interpolant_cuadratic_class_1(
+        left: f32,
+        right: f32,
+        step_long: f32,
+        function: String,
+    ) -> Vec<F2DPoint> {
+        todo!()
+    }
+
+    fn interpolant_cubic_class_2(
+        left: f32,
+        right: f32,
+        step_long: f32,
+        function: String,
+    ) -> Vec<F2DPoint> {
+        todo!()
+    }
 }
--- src/surfaces.rs
+++ src/surfaces.rs
@@ -1,18 +1,168 @@
-use crate::curves::Point2D;
-
-pub struct Point3D {
+pub struct F3DPoint {
     x: f32,
     y: f32,
     z: f32,
 }
-trait BezierSurface {
-    fn decasteljau_surfaces(points: &[Point2D]) -> Vec<Point2D>;
-    fn degree_elevation(points: &[Point2D], degree: i32) -> Vec<Point2D>;
-    fn interpolant_surfaces(
-        left: f32,
-        right: f32,
-        step_long: f32,
-        function: fn(f32) -> f32,
-    ) -> Vec<Point3D>;
-    // easy on the surfaces one, there is a lot of things to define first
+//trait BezierSurface {
+//    fn decasteljau_surfaces(points: &[2DPoint]) -> &[2DPoint]
+//    fn degree_elevation(points: &[2DPoint], degree: i32) -> &[2DPoint]
+//    fn interpolant_surfaces(left: f32, right: f32, step_long: f32, function: String) -> &[3DPoint]
+// easy on the surfaces one, there is a lot of things to define first
+//}
+
+fn Vij(i: f32, j: f32, h: f32) -> (f32, f32) {
+    return (i * h + j * h, i * h - j * h);
+}
+
+fn uij11(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i + 1.0, j + 1.0, h);
+    return ((3.0 * vij.0 + vij1.0) / 4.0, (3.0 * vij.1 + vij1.1) / 4.0);
+}
+
+fn uij10(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i + 1.0, j, h);
+    return ((3.0 * vij.0 + vij1.0) / 4.0, (3.0 * vij.1 + vij1.1) / 4.0);
+}
+
+fn uij0minus1(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i, j - 1.0, h);
+    return ((3.0 * vij.0 + vij1.0) / 4.0, (3.0 * vij.1 + vij1.1) / 4.0);
+}
+fn uijminus1minus1(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i - 1.0, j - 1.0, h);
+    return ((3.0 * vij.0 + vij1.0) / 4.0, (3.0 * vij.1 + vij1.1) / 4.0);
+}
+fn uijminus10(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i - 1.0, j, h);
+    return ((3.0 * vij.0 + vij1.0) / 4.0, (3.0 * vij.1 + vij1.1) / 4.0);
+}
+fn uij01(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i, j + 1.0, h);
+    return ((3.0 * vij.0 + vij1.0) / 4.0, (3.0 * vij.1 + vij1.1) / 4.0);
+}
+
+fn eij10(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i + 1.0, j, h);
+    return ((vij.0 + vij1.0) / 2.0, (vij.1 + vij1.1) / 2.0);
+}
+
+fn eij11(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i + 1.0, j + 1.0, h);
+    return ((vij.0 + vij1.0) / 2.0, (vij.1 + vij1.1) / 2.0);
+}
+fn eij01(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i, j + 1.0, h);
+    return ((vij.0 + vij1.0) / 2.0, (vij.1 + vij1.1) / 2.0);
+}
+
+fn zij11(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i + 1.0, j + 1.0, h);
+    let vij2 = Vij(i + 1.0, j, h);
+    return (
+        (2.0 * vij.0 + vij1.0 + vij2.0) / 4.0,
+        (2.0 * vij.1 + vij1.1 + vij2.1) / 4.0,
+    );
+}
+fn zij10(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i + 1.0, j, h);
+    let vij2 = Vij(i, j - 1.0, h);
+    return (
+        (2.0 * vij.0 + vij1.0 + vij2.0) / 4.0,
+        (2.0 * vij.1 + vij1.1 + vij2.1) / 4.0,
+    );
+}
+fn zij0minus1(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i, j - 1.0, h);
+    let vij2 = Vij(i - 1.0, j - 1.0, h);
+    return (
+        (2.0 * vij.0 + vij1.0 + vij2.0) / 4.0,
+        (2.0 * vij.1 + vij1.1 + vij2.1) / 4.0,
+    );
+}
+fn zijminus1minus1(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i - 1.0, j - 1.0, h);
+    let vij2 = Vij(i - 1.0, j, h);
+    return (
+        (2.0 * vij.0 + vij1.0 + vij2.0) / 4.0,
+        (2.0 * vij.1 + vij1.1 + vij2.1) / 4.0,
+    );
+}
+fn zijminus10(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i - 1.0, j, h);
+    let vij2 = Vij(i, j + 1.0, h);
+    return (
+        (2.0 * vij.0 + vij1.0 + vij2.0) / 4.0,
+        (2.0 * vij.1 + vij1.1 + vij2.1) / 4.0,
+    );
+}
+fn zij01(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i, j + 1.0, h);
+    let vij2 = Vij(i + 1.0, j + 1.0, h);
+    return (
+        (2.0 * vij.0 + vij1.0 + vij2.0) / 4.0,
+        (2.0 * vij.1 + vij1.1 + vij2.1) / 4.0,
+    );
+}
+fn tij(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i + 1.0, j + 1.0, h);
+    let vij2 = Vij(i + 1.0, j, h);
+    return (
+        (vij.0 + vij1.0 + vij2.0) / 3.0,
+        (vij.1 + vij1.1 + vij2.1) / 3.0,
+    );
+}
+fn bij(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i + 1.0, j + 1.0, h);
+    let vij2 = Vij(i, j + 1.0, h);
+    return (
+        (vij.0 + vij1.0 + vij2.0) / 3.0,
+        (vij.1 + vij1.1 + vij2.1) / 3.0,
+    );
+}
+fn wij11(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i + 1.0, j + 1.0, h);
+    return ((2.0 * vij.0 + vij1.0) / 3.0, (2.0 * vij.1 + vij1.1) / 3.0);
+}
+fn wij10(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i + 1.0, j, h);
+    return ((2.0 * vij.0 + vij1.0) / 3.0, (2.0 * vij.1 + vij1.1) / 3.0);
+}
+fn wij0minus1(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i, j - 1.0, h);
+    return ((2.0 * vij.0 + vij1.0) / 3.0, (2.0 * vij.1 + vij1.1) / 3.0);
+}
+fn wijminus1minus1(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i - 1.0, j - 1.0, h);
+    return ((2.0 * vij.0 + vij1.0) / 3.0, (2.0 * vij.1 + vij1.1) / 3.0);
+}
+fn wijminus10(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i - 1.0, j, h);
+    return ((2.0 * vij.0 + vij1.0) / 3.0, (2.0 * vij.1 + vij1.1) / 3.0);
+}
+fn wij01(i: f32, j: f32, h: f32) -> (f32, f32) {
+    let vij = Vij(i, j, h);
+    let vij1 = Vij(i, j + 1.0, h);
+    return ((2.0 * vij.0 + vij1.0) / 3.0, (2.0 * vij.1 + vij1.1) / 3.0);
 }
